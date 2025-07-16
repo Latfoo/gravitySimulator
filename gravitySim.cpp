18cf66cdd6d5ea6e5cb10d691e7dacf0123a5b06
Latfoo <138173383+Latfoo@users.noreply.github.com>
2025-07-16 17:06:24 +0200

    Applied basic kinematics with fixed earth acceleration to test movement

--- gravitySim.cpp
+++ gravitySim.cpp
@@ -8,13 +8,13 @@ typedef struct vector{
     float y;
 }vector;
 
-void drawCircle(float centerX = SCREEN_WIDTH / 2.0f, float centerY = SCREEN_HEIGHT / 2.0f, float radius = 50, int res = 100){
+void drawCircle(float centerX , float centerY , float radius, int res){
 
     glBegin(GL_TRIANGLE_FAN);
     glVertex2d(centerX,centerY);
 
     for(int i = 0; i < res+1; i++){
-            float angle = 2.0f * M_PI * (float)i/res;
+            float angle = 2.0f * M_PI * static_cast<float>(i)/res;
             float x = centerX + cos(angle) * radius;
             float y = centerY + sin(angle) * radius;
             glVertex2d(x,y);
@@ -41,21 +41,47 @@ int main() {
     glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, -1, 1);  // orthographic projection
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-
     glClearColor(0, 0, 0, 1);
 
-    //Running the simulation
+    // --------------------- Simulation -------------------------
+
+    // Parameters
+    int res = 100;
+    int radius = 50;
+    vector position = { SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f };
+    vector velocity = { 0.0f, 0.0f };
+    vector acceleration = {0.0f, -9.81f}; //Earth
+
+    float lastTime = glfwGetTime();
+
+    // Rendering Loop
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        drawCircle();
+        //Time
+        float currentTime = glfwGetTime();
+        float deltaTime = currentTime - lastTime;
+        lastTime = currentTime;
+
+        // Numerical Integration using semi-implicit Euler
+        velocity.x += acceleration.x * deltaTime;
+        velocity.y += acceleration.y * deltaTime;
+
+        position.x += velocity.x * deltaTime;
+        position.y += velocity.y * deltaTime;
+
+        // Edge Protection
+        if (position.x < radius || position.x > SCREEN_WIDTH - radius) velocity.x *= -1;
+        if (position.y < radius || position.y > SCREEN_HEIGHT - radius) velocity.y *= -1;
+
+        drawCircle(position.x, position.y, radius, res);
         
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // Close window
+    // --------------------- END of Simulation -------------------------
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
