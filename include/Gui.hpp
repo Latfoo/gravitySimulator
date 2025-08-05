3a6a0a7ad88c543a26583665a8e78b001f17e432
Latfoo <138173383+Latfoo@users.noreply.github.com>
2025-08-06 00:09:07 +0200

    Add descriptions to Gui functions in Gui.hpp
    Add descriptions to Gui functions in Gui.hpp (for real this time).

--- include/Gui.hpp
+++ include/Gui.hpp
@@ -2,7 +2,28 @@
 #include <GLFW/glfw3.h>
 struct SimulationState;
 
+/**
+ * @brief Initialize the ImGui and ImPlot contexts.
+ * 
+ * @param window Pointer to the GLFW window where the GUI will be rendered.
+ */
 void GuiInit(GLFWwindow* window);
+
+/**
+ * @brief Start a new ImGui frame.
+ */
 void GuiNewFrame();
+
+/**
+ * @brief Build and render the GUI for the current frame.
+ * 
+ * @param simstate Reference to the SimulationState.
+ * @param dt_ms    Delta time for the last frame in milliseconds.
+ * @param paused   Reference to the simulation pause boolean, allowing toggling physics on and off.
+ */
 void GuiRender(SimulationState& simstate, float dt_ms, bool& paused);
+
+/**
+ * @brief Clean up ImGui and ImPlot contexts.
+ */
 void GuiShutdown();
